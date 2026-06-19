a7291aa744163f6b229882e279d3fc5cfa36988f
shikajiko <riyanfadli675@gmail.com>
2026-06-19 23:32:26 +0700

    change node name

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    const auto node = std::make_shared<rclcpp::Node>("action_manager");
+    const auto node = std::make_shared<rclcpp::Node>("booster_action_manager");
     const booster_action_manager::ActionManagerNode action_manager_node(node);
     rclcpp::spin(node);
     rclcpp::shutdown();
